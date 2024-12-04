47295d7298cbac87867ba11271a3ae8befed7d46
Asadshaik17 <shaik_mohammad2023@cms.ac.in>
2024-12-04 18:52:49 +0530

    Untill Connected to MongoDB and created models folder & files

---
+++ index.js
@@ -0,0 +1,15 @@
+const Joi = require('joi');
+const genres = require('./routes/genres');
+const customers = require('./routes/customers');
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/vidly');
+
+app.use(express.json());
+app.use('/api/genres', genres);
+app.use('/api/customers', customers);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
